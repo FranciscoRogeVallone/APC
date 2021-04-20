a269454192de59e4ab9f71f8d0d23eacb836e8cc
FranciscoRogeVallone <82470439+FranciscoRogeVallone@users.noreply.github.com>
2021-04-20 18:26:54 -0300

    Add files via upload

--- APC.py
+++ APC.py
@@ -1,31 +1,129 @@
 from tkinter import * 
+from tkinter import filedialog
 from tkinter.ttk import Progressbar , Combobox , Treeview
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import soundfile as sf
 
 
+def func_calculate():
+    if len(RIRs)==0:
+        return
+    if str.isdigit(ent_winlen.get())==0 and cmbx_envelope.current()!=0:
+        return
+    
+    resultados = np.zeros([len(parameters),len(bands[cmbx_bandfilter.current()])+1,len(RIRs)])
+    signals = [[]]
+    envelopes = [[]]
+    for s in range(len(RIRs)):
+        for b in range(len(bands[cmbx_bandfilter.current()])+1):
+            if b==0:
+                winlen = 10*np.int(fs[s]/bands[0])
+                signals[s][b] = RIRs[s]
+            else:
+                winlen = 10*np.int(fs[s]/bands[b-1])
+                if reversedcheck.get()==1:
+                    print(6)#flip filtrar flip
+                else:
+                    print(8)#filtrar
+            
+            signals[s][b] = 20*np.log10(np.abs(signals[s][b]))
+            IRstart = np.where(signals[s][b][0:np.argmax(signals[s][b])]<=np.max(signals[s][b])/10)[0][-1]
+            signals[s][b] = signals[s][b][IRstart:]
+            
+            if cmbx_IRplt.current()==0:
+                print(lundeby)
+            elif cmbx_IRplt.current()==1:
+                print(pepino)
+            elif cmbx_IRplt.current()==2:
+                [IRend,meanmf] = metodo_propio(signals[s][b],fs[s],winlen)
+            
+            if cmbx_envelope.current()==0:
+                print(schoreder)
+            elif cmbx_envelope.current()==1:
+                if IRend+1+winlen<len(signals[s][b]):
+                    env = ndimage.median_filter(signals[s][b][0:IRend+1+winlen],winlen)
+                else:
+                    env = ndimage.median_filter(signals[s][b],winlen)
+                env = env[int(winlen/2):int(len(env)-winlen/2)]
+            elif cmbx_envelope.current()==2:
+                if cmbx_IRplt.current()==2:
+                    env = env[0:IRend+1]
+                else:
+                    if IRend+1+winlen<len(signals[s][b]):
+                        env = mediamovil(signals[s][b][0:IRend+winlen],winlen)
+                    else:
+                        env = mediamovil(signals[s][b],winlen)
+            envelopes[s] = list.append(envelopes[s],env)
+            
+            edtsamples = getsamplesbetween(envelopes[s][b],0,-10)
+            resultados[0,b,s] = 60/(slope(edtsamples)*fs[s])
+            
+            t20samples = getsamplesbetween(envelopes[s][b],-5,-25)
+            resultados[1,b,s] = 60/(slope(t20samples)*fs[s])
+            
+            t30samples = getsamplesbetween(envelopes[s][b],-5,-35)
+            resultados[2,b,s] = 60/(slope(t30samples)*fs[s])
+            
+            c50 =
+            
+            c80 = 
+    return
+
+def slope(data):
+    n = len(data)
+    x = np.arange(0,n)
+    m = ( n*np.sum(x*data)+np.sum(x)*np.sum(data) ) / ( n*np.sum(x**2)-np.sum(x)**2 )
+    return m
+
+def getsamplesbetween(data,Ls,Li):
+    sup = np.where(data>=np.max(data)+Ls)[0][-1]
+    inf = np.where(data<np.max(data)+Li)[0][0]
+    samples = data[sup+1:inf]
+    return samples
+
+def mediamovil(data,k):
+    w = np.concatenate([np.zeros([len(data)-1]),np.ones([k])/k])
+    data = np.concatenate([data,np.zeros([k-1])])
+    fftdata = np.fft.rfft(data)
+    fftw = np.fft.rfft(w)
+    mmf = np.fft.irfft(fftdata*fftw)
+    mmf = mmf[0:len(data)-k+1]
+    return mmf
+
+def metodo_propio(data,fs,k):
+    
+    #media movil
+    mmf=mediamovil(data,k)
+    #recta
+    mmf = np.concatenate([mmf,np.ones([fs])*mmf[-1]])
+    indexmax = np.argmax(mmf)
+    levelmax = np.max(mmf)
+    M = (mmf[-1]-levelmax)/(len(mmf)-indexmax)
+    B = (levelmax-M*indexmax)
+    cut = np.argmax(M*range(len(mmf))[indexmax:]+B-mmf[indexmax:])+k/2+indexmax
+    
+    return cut , mmf
+
 def func_load():
-    name = filedialog.askopenfilenames()
-    global signals
-    global fs
-    signals=[]
-    fs=[]
+    name = filedialog.askopenfilenames(title="Load RIRs files",filetypes=[("WAV Audio",".wav")])
     for k in range(len(name)):
         lstbx_IRs.insert("end",name[k])
         [sgn,frec]=sf.read(name[k])
-        signals=signals+[sgn]
-        fs=fs+[frec]
+        list.append(RIRs,sgn)
+        list.append(fs,frec)
     return
 
 def func_clearall():
-    signals=[]
-    fs=[]
+    list.clear(RIRs)
+    list.clear(fs)
     lstbx_IRs.delete(0,"end")
     return
 
 def func_clearselected():
     for k in lstbx_IRs.curselection():
+        list.pop(RIRs,lstbx_IRs.curselection()[0])
+        list.pop(fs,lstbx_IRs.curselection()[0])
         lstbx_IRs.delete(lstbx_IRs.curselection()[0])
     return
 
@@ -56,14 +154,21 @@ def hidesettings(event):
     if cmbx_envelope.current()==0:
         ent_winlen.grid_remove()
         lbl_winlen.grid_remove()
-        lbl_noisedetector.grid()
-        cmbx_noisedetector.grid()
+        lbl_chunk.grid()
+        cmbx_chunk.grid()
     else:
         ent_winlen.grid()
         lbl_winlen.grid()
-        lbl_noisedetector.grid_remove()   
-        cmbx_noisedetector.grid_remove()
+        lbl_chunk.grid_remove()   
+        cmbx_chunk.grid_remove()
     return
+
+RIRs=[]
+fs=[]
+bands=[(63,125,250,500,1000,2000,4000,8000),("50","63","80","100","125","160","200","250","315","400","500","630","800","1k","1.25k","1.6k","2k","2.5k","3.15k","4k","5k","6.3k","8k","10k")]
+bands_labels=[("63","125","250","500","1k","2k","4k","8k"),("50","63","80","100","125","160","200","250","315","400","500","630","800","1k","1.25k","1.6k","2k","2.5k","3.15k","4k","5k","6.3k","8k","10k")]
+parameters=("EDT", "T20", "T30", "C50", "C80", "Tt", "EDTt")
+filas1=("Average","Max","Min","Sigma")
 
 APC = Tk()
 APC.title("Acoustic Parameters Calculator")
@@ -111,7 +216,7 @@ btn_clear.grid(row=1, column=0, padx=2, pady=2, sticky="ewns")
 btn_clear = Button(frame_menu, text="Clear selected", command=func_clearselected)
 btn_clear.grid(row=2, column=0, padx=2, pady=2, sticky="ewns")
 
-btn_calculate = Button(frame_menu, text="Calculate", font=("Arial",12,"bold"))
+btn_calculate = Button(frame_menu, text="Calculate", font=("Arial",12,"bold"),command=func_calculate)
 btn_calculate.grid(row=4, column=0,columnspan=3, sticky="ewns", padx=2, pady=2)
 
 btn_show = Button(frame_menu, text="Show average", command=func_show)
@@ -178,26 +283,24 @@ ent_winlen = Entry(frame_settings,width=10)
 ent_winlen.grid(row=3, column=1, sticky="ewns",pady=1)
 ent_winlen.grid_remove()
 
-lbl_noisedetector = Label(frame_settings, text="Noise Detector:", anchor="e")
-lbl_noisedetector.grid(row=3, column=0, sticky="ewns")
+lbl_chunk = Label(frame_settings, text="IR chunk:", anchor="e")
+lbl_chunk.grid(row=3, column=0, sticky="ewns")
 
-cmbx_noisedetector = Combobox(frame_settings, values=("Lundeby", "Pepino", "SSM"), state="readonly", width=10)
-cmbx_noisedetector.current(0)
-cmbx_noisedetector.grid(row=3, column=1, sticky="ewns", pady=1)
+cmbx_chunk = Combobox(frame_settings, values=("Lundeby", "Pepino", "K0"), state="readonly", width=10)
+cmbx_chunk.current(0)
+cmbx_chunk.grid(row=3, column=1, sticky="ewns", pady=1)
 
-chkbtn_reverse = Checkbutton(frame_settings, text="Reversed Filtering")
+reversedcheck = IntVar()
+chkbtn_reverse = Checkbutton(frame_settings, text="Reversed Filtering", variable=reversedcheck)
 chkbtn_reverse.grid(row=4, column=0, columnspan=2, sticky="ewns")
 
-bands=[("63","125","250","500","1k","2k","4k","8k"),("50","63","80","100","125","160","200","250","315","400","500","630","800","1k","1.25k","1.6k","2k","2.5k","3.15k","4k","5k","6.3k","8k","10k")]
-parameters=("EDT", "T20", "T30", "C50", "C80", "Tt", "EDTt")
-filas1=("Average","Max","Min","Sigma")
 frame_table1 = Frame(APC)
 frame_table1.grid(row=1,column=0,columnspan=2,padx=4,pady=4,sticky="ewns")
 frame_table1.grid_remove()
 for i in range(len(filas1)+1):
-    for j in range(len(bands[1])+1):
+    for j in range(len(bands_labels[1])+1):
         if i==0 and j!=0:
-            table1[i] = table1[i] + [Label(frame_table1, text=bands[1][j-1], bg="white", relief="solid",borderwidth=1)]
+            table1[i] = table1[i] + [Label(frame_table1, text=bands_labels[1][j-1], bg="white", relief="solid",borderwidth=1)]
             table1[i][j].grid(row=i,column=j,sticky="ewns")
             frame_table1.columnconfigure(j, weight=1)
         elif i!=0 and j==0:
@@ -214,9 +317,9 @@ for i in range(len(filas1)+1):
 frame_table2 = Frame(APC)
 frame_table2.grid(row=1,column=0,columnspan=2,padx=4,pady=4,sticky="ewns")
 for i in range(len(parameters)+1):
-    for j in range(len(bands[1])+1):
+    for j in range(len(bands_labels[1])+1):
         if i==0 and j!=0:
-            table2[i] = table2[i] + [Label(frame_table2, text=bands[1][j-1], bg="white", relief="solid",borderwidth=1)]
+            table2[i] = table2[i] + [Label(frame_table2, text=bands_labels[1][j-1], bg="white", relief="solid",borderwidth=1)]
             table2[i][j].grid(row=i,column=j,sticky="ewns")
             frame_table2.columnconfigure(j, weight=1)
         elif i!=0 and j==0:
